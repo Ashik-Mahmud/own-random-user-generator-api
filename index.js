e29d55ec4c9e0e4f3f916344576edc104ff148d2
ashikmahmud <ashikmamud187@gmail.com>
2022-09-04 03:23:12 +0600

    added port number dynamically

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const users = require("./utils/FileSystem.js");
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 // importedRoutes 

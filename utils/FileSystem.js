721f74e8f075ff277ceaf4e44e4d7ea47c3b0163
ashikmahmud <ashikmamud187@gmail.com>
2022-09-04 15:33:29 +0600

    change path system

--- utils/FileSystem.js
+++ utils/FileSystem.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 const path = require("path")
 
+const dataPath = "Users.json";
 
-const usersBuffer = fs.readFileSync(path.join(__dirname, "../views/Users.json"));
+const usersBuffer = fs.readFileSync(dataPath);
 const users = JSON.parse(usersBuffer);
+
 
 /* /* Adding Single New User /*  */
 const addingNewUser = (newUser) => {
    try {
 
-    const usersBuffer = fs.readFileSync(path.join(__dirname, "../views/Users.json"));
+    const usersBuffer = fs.readFileSync(dataPath);
     const users = JSON.parse(usersBuffer);
     users.push(newUser);
     const sortedByID = users.sort((a, b) => a.id - b.id);
-    fs.writeFileSync(path.join(__dirname, "../views/Users.json"), JSON.stringify(sortedByID));
+    fs.writeFileSync(dataPath, JSON.stringify(sortedByID));
 
    } catch (error) {
        console.log(error.message);
@@ -25,7 +27,7 @@ const addingNewUser = (newUser) => {
 const usersExceptDeletedItem = (users) => {
     try {
      const sortedByID = users.sort((a, b) => a.id - b.id);
-     fs.writeFileSync(path.join(__dirname, "../views/Users.json"), JSON.stringify(sortedByID));
+     fs.writeFileSync(dataPath, JSON.stringify(sortedByID));
  
     } catch (error) {
         console.log(error.message);

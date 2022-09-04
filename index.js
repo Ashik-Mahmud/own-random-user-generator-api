52fece59b84a739b41094b97a22d012e71a80119
ashikmahmud <ashikmamud187@gmail.com>
2022-09-04 15:16:09 +0600

    change packge.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const users = require("./utils/FileSystem.js");
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 
 // importedRoutes 
